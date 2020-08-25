573411cb85fba8d759efce8aab5d1e6e99bb68de
manjusha <manju.unni85@gmail.com>
2020-08-25 09:27:58 -0700

    Final commit

---
+++ src/test/java/MavenProjects/MavenJavaProjects/Demo.java
@@ -0,0 +1,13 @@
+package MavenProjects.MavenJavaProjects;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void demoTestCase(){
+		System.out.println("demoTestCase1");
+		System.out.println("demoTestCase2");
+		System.out.println("demoTestCase3");
+	}
+}
