673b832f8ab23fa3118319edef08fc48611edf95
manjusha <manju.unni85@gmail.com>
2020-08-24 23:57:15 -0700

    second commit

--- src/test/java/MavenProjects/MavenJavaProjects/RestAPITest.java
+++ src/test/java/MavenProjects/MavenJavaProjects/RestAPITest.java
@@ -7,11 +7,13 @@ public class RestAPITest {
 	@Test
 	public void restAPITestCase1(){
 		System.out.println("RestAPITestCase1");
+		System.out.println("RestAPITestCase2");
+		System.out.println("RestAPITestCase3");
 	}
 	
 	@Test
 	public void RestAPITestCase2(){
-		System.out.println("RestAPITestCase2");
+		System.out.println("RestAPITestCase4");
 	}
 
 }
